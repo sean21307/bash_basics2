5da22232101e7e5d30d6b23e071cf8e63ddb0866
Sean Nolan <seannolan21307@gmail.com>
2024-03-31 17:01:14 -0400

    added to read_input script

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,7 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+echo 'Enter a file name'
+read filename
+touch $filename.txt
+echo "Created file named $filename"
